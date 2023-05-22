c7fa37953228733b3952e978b3194b21bd608b12
Josh Bowen <josh9398@hotmail.com>
2023-05-22 20:06:07 +0100

    Add initial demo next.js shadcn ui app

---
+++ app/page.tsx
@@ -0,0 +1,114 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+          Images<br className="hidden sm:inline" />
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
+          Deployed into your cluster since: TODO DATE
+        </p>
+      </div>
+      <div className="flex gap-2">
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.invoice}>
+                <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                <TableCell>{invoice.paymentStatus}</TableCell>
+                <TableCell>{invoice.paymentMethod}</TableCell>
+                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants({ size: "lg" })}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline", size: "lg" })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "AirGap UI",
+  description: "Frontend UI for ImperialOps AirGap.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Poo",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/ImperialOps",
+    github: "https://github.com/ImperialOps",
+    docs: "https://ui.shadcn.com",
+  },
+}
